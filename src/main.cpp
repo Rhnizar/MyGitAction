69a2c05fba57541d0a11d07215963e146c102046
Rida Rhnizar <ridarhnizar1337@gmail.com>
2024-11-23 13:13:42 +0100

    Make Repository Structure

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
+
+int main() {
+    std::cout << "Hello, GitHub Actions!" << std::endl;
+    return 0;
+}
---
+++ tests/test.cpp
@@ -0,0 +1,29 @@
+#include "gtest/gtest.h"
+#include "main.cpp"
+
+// Test for add function
+TEST(AdditionTest, HandlesPositiveNumbers) {
+    EXPECT_EQ(add(3, 5), 8);
+    EXPECT_EQ(add(10, 20), 30);
+}
+
+TEST(AdditionTest, HandlesNegativeNumbers) {
+    EXPECT_EQ(add(-3, -5), -8);
+    EXPECT_EQ(add(-10, 20), 10);
+}
+
+// Test for subtract function
+TEST(SubtractionTest, HandlesPositiveNumbers) {
+    EXPECT_EQ(subtract(10, 5), 5);
+    EXPECT_EQ(subtract(20, 30), -10);
+}
+
+TEST(SubtractionTest, HandlesNegativeNumbers) {
+    EXPECT_EQ(subtract(-10, -5), -5);
+    EXPECT_EQ(subtract(-20, 10), -30);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
